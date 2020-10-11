7964f37c0db69258b45882068e945c5acd42b1b9
Derevianko Liia <tiwebli@gmail.com>
2020-10-11 19:04:32 +0300

    Completed stages
    1)Создание и рендер разметки по массиву данных и предоставленному шаблону.
    2)Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
    3)Открытие модального окна по клику на элементе галереи.
    4)Подмена значения атрибута src элемента img.lightbox__image.
    5)Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
    6)Очистка значения атрибута src элемента img.lightbox__image.

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,20 @@
 import images from './gallery-items.js';
 
-console.log();
-
 const containerImages = document.querySelector('.js-gallery');
 const galleryImages = createGallaryImages(images);
 containerImages.insertAdjacentHTML('afterbegin', galleryImages);
+containerImages.addEventListener('click', onImageContainerClick)
+
+const containerModalOpen = document.querySelector('div.lightbox');
+const modalOpenImage = document.querySelector('.lightbox__image')
+
+const overlayModalClick = document.querySelector('.lightbox__overlay')
+const buttonModalClick = document.querySelector('.lightbox__button')
+
+const containerModalClose = document.querySelector('button[data-action="close-lightbox"]')
+
+overlayModalClick.addEventListener('click', onOverlayAndBtnClick)
+buttonModalClick.addEventListener('click', onOverlayAndBtnClick)
 
 function createGallaryImages(images) {
   return images.map(({ preview, original, description }) => {
@@ -24,7 +34,63 @@ function createGallaryImages(images) {
   }).join('');
 }
 
-{/* <li class="gallery__item">
+function onImageContainerClick(evt) {
+  evt.preventDefault();
+  
+  const isGalleryEl = evt.target.classList.contains('js-gallery');
+  if (isGalleryEl) {
+    return;
+  }
+  // console.log('target', evt.target);
+  // console.log('currentTarget', evt.currentTarget);
+
+  onOpenModalClick()
+  addModalImage(evt)
+  // previousImage(e)
+}
+
+function onOpenModalClick() {
+  containerModalOpen.classList.add('is-open');
+}
+
+function addModalImage(evt) {
+  modalOpenImage.src = evt.target.dataset.source;
+  modalOpenImage.alt = evt.target.alt;
+}
+
+function onOverlayAndBtnClick(evt) {
+  if (evt.target === overlayModalClick || evt.target === containerModalClose) {
+    onCloseModal();
+  }
+}
+
+function onCloseModal() {
+  // window.removeEventListener(‘keydown’, onEscCloseModal);
+  // galleryListRef.removeEventListener(‘keydown’, onClickImageSlider);
+  containerModalOpen.classList.remove('is-open');
+    clearLightBoxImage();
+} 
+
+function clearLightBoxImage() {
+  modalOpenImage.removeAttribute("src");
+  modalOpenImage.removeAttribute("alt");
+}
+
+function previousImage(e) {
+  if (!(e.key === "ArrowLeft")) {
+    return;
+  }
+  let index = items.findIndex((el) => {
+    return el.original === modalOpenImage.getAttribute("src");
+  });
+  if (index === 0) {
+    index = items.length;
+  }
+  index -= 1;
+  const previousPic = items[index].original;
+  modalOpenImage.setAttribute("src", previousPic);
+}
+/* <li class="gallery__item">
   <a
     class="gallery__link"
     href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
@@ -36,4 +102,17 @@ function createGallaryImages(images) {
       alt="Tulips"
     />
   </a>
-</li> */}
+</li> */
+
+
+// function onOverlayAndBtnClick(e) {
+//   if (e.target === lightboxOverlayRef(ссылка на оверлей) || e.target === modalCloseBtnRef(ссылка на кнопку закрытия через дата атрибут)) {
+//     onCloseModal();
+//   }
+// }
+// function onCloseModal() {
+//   // window.removeEventListener(‘keydown’, onEscCloseModal);
+//   // galleryListRef.removeEventListener(‘keydown’, onClickImageSlider);
+//   lightboxRef.classList.remove(‘is-open’);
+//   unsetImageAttributes();
+// } 
